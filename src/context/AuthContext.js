70a2d60e8f9e2ffa59f83e5292b1e4065c148175
BarunBlog <bhattacharjeebarun25@gmail.com>
2023-07-23 21:13:45 +0600

    changed proxy settings

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -2,26 +2,7 @@ import { createContext, useReducer } from "react";
 import AuthReducer from "./AuthReducer";
 
 const INITIAL_STATE = {
-    user: {
-        "_id": "647b4e4c899432f5fb811ca9",
-        "username": "TestUser",
-        "email": "testuser@gmail.com",
-        "password": "$2b$10$gdqt2rkIeS3nrEh/LjAfBO41L6z1LBqclwOjuWMLjOvkdqkDIaVMK",
-        "profilePicture": "https://e0.pxfuel.com/wallpapers/39/255/desktop-wallpaper-stylish-boys-cool-profile-pics-dp-for-facebook-whatsapp-2019-boy-smoke.jpg",
-        "coverPicture": "",
-        "followers": [
-            "647e20f95a23287b0efbb838",
-            "6482130ff0018e511ed5e3dc"
-        ],
-        "isAdmin": false,
-        "createdAt": "2023-06-03T14:29:32.677Z",
-        "updatedAt": "2023-07-15T13:34:41.593Z",
-        "__v": 6,
-        "followings": [
-            "6482130ff0018e511ed5e3dc",
-            "647e20f95a23287b0efbb838"
-        ]
-    },
+    user: null,
     isFetching: false,
     error: false
 };
--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import App from './App';
 import { AuthContextProvider } from './context/AuthContext';
 import { disableReactDevTools } from '@fvilers/disable-react-devtools'
 
-if (ProcessingInstruction.env.NODE_ENV === 'production') disableReactDevTools()
+if (process.env.NODE_ENV === 'production') disableReactDevTools()
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
